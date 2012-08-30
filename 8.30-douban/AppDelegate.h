5fa5acfe9a33543c4e889e544dca86c387e05ec9
4DWORK <4dwork@brandy.local>
2012-08-30 15:18:42 +0800

    Initial Commit

---
+++ 8.30-douban/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  8.30-douban
+//
+//  Created by 4DWORK on 12-8-30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
